f9d931f944b2a87a8225d22522fc705a70fdda5b
ashiful islam <ashifulislam2002@gmail.com>
2025-05-23 01:23:40 +0600

    implement recipe get and post method

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-thid ifd
+## BOOK APP SERVER
+// recipe book user : ashifulislam2002
+// recipe book pass : 4wTd91BPeL5qXCiw
---
+++ index.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // database create for users
+    const database = client.db("usersdb");
+    const usersCollection = database.collection("users");
+
+    // creating databse for recipe
+    const recipedb = client.db("recipedb");
+    const recipeCollection = recipedb.collection("recipe");
+
+    // get users from databser
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // post users to database
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    //  recipe related database
+
+    // get recipe form  databse
+    app.get("/recipes", async (req, res) => {
+      const cursor = recipeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // to receive data from database and send to frontend
+
+
+
+    
+    app.get("/recipes/:id", async (req, res) => {
+      const id = req.params.id;
+      try {
+        const recipe = await recipeCollection.findOne({
+          _id: new ObjectId(id),
+        });
+        if (!recipe) {
+          return res.status(404).send({ error: "Recipe not found" });
+        } else {
+          res.send(recipe);
+        }
+      } catch (error) {
+        res.status(500).send({ error: "server error" });
+      }
+      //   console.log(id);
+    });
+
+    // post users data to database
+    app.post("/recipes", async (req, res) => {
+      const newRecipe = req.body;
+      const result = await recipeCollection.insertOne(newRecipe);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Root
+app.get("/", (req, res) => {
+  res.send("Hello World this is working !");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
