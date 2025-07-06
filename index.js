6d123885261fa325ec7bcbbc24d5c6752d2a5a4a
ashiful islam <ashifulislam2002@gmail.com>
2025-07-06 13:52:24 +0600

    removed form ph repo

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     // database create for users
     const database = client.db("usersdb");
@@ -159,7 +159,7 @@ async function run() {
     });
 
     //////////
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
